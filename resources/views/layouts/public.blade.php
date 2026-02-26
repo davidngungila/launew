64a9c7d7bbbb4d0b74e3966034f820f76d28bfb7
Ngungila <168288981+Ngungila@users.noreply.github.com>
2026-02-26 08:51:08 +0300

    Add Google Analytics gtag

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -5,6 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>LAU Paradise Adventure | Discover the Wild</title>
     <link rel="icon" type="image/png" href="{{ asset('lau-adventuress-logo.png') }}">
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-09B92CP2SN"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-09B92CP2SN');
+    </script>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@300;400;500;600;700;800&family=Playfair+Display:wght@700;900&display=swap" rel="stylesheet">
