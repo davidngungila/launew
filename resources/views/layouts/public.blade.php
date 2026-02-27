e359d1393637ad33557fb3f8931084852bda8675
Ngungila <168288981+Ngungila@users.noreply.github.com>
2026-02-27 12:43:21 +0300

    Ensure GTM fallback container ID on public pages

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -7,6 +7,7 @@
     <link rel="icon" type="image/png" href="{{ asset('lau-adventuress-logo.png') }}">
     @php($integrations = \App\Models\SystemSetting::getValue('integrations', []))
     @php($gtmId = (string) data_get($integrations, 'gtm_container_id', ''))
+    @php($gtmId = $gtmId ?: 'GTM-WC2NH5GF')
     @php($ga4Id = (string) data_get($integrations, 'ga4_measurement_id', ''))
     @if($gtmId)
         <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','{{ $gtmId }}');</script>
